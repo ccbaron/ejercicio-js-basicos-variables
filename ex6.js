2470bdf84b900c8e534a36d518d1a6e7701e5092
ccbaron <christian7baron@gmail.com>
2025-03-05 21:45:34 +0100

    ex6 OK

--- ex6.js
+++ ex6.js
@@ -13,7 +13,9 @@
  */
 
 let numeroPi = Math.PI
-console.log("Valor de PI:", numeroPi)
+let radio = 5
+let area = Math.PI * (radio * 2)
+console.log("El area de un circulo de radio", radio, "es de", area, "m2")
 
 /**
  * BONUS: Muestra el mensaje de tal manera que el número devuelto esté redondeado a dos decimales. Es decir, para un radio de 3, el mensaje debería ser: ""El area de un círculo de radio 3 es de 28.27 m2"
