8dbd577265e805a40b9b5a68dc4ab72b77c1136f
ccbaron <christian7baron@gmail.com>
2025-03-05 22:01:34 +0100

    ex7 OK

--- ex7.js
+++ ex7.js
@@ -15,5 +15,5 @@ let numJuguetes = 5;
 let numHorasSueño = 16;
 let jugandoAhora = "pelota";
 
-let historiaGato; // modificar solamente  esta línea
+let historiaGato = "Mi gato se llama" + " " + nombreGato + ", tiene" + " " + edadGato + " años y pesa " + pesoGato + " es de color " + colorGato + " y tiene una " + personalidadGato + ". Le encanta comer " + comidaFavorita + " y tiene " + numJuguetes + " diferentes. Necesita dormir al menos" + numHorasSueño + " horas al día. Ahora mismo está jugando con su " + jugandoAhora + ". ¡Es un gato muy feliz!"  ; // modificar solamente  esta línea
 console.log(historiaGato);
